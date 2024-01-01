370c3fe6a88eff6811d5008b2db57d6031022a30
Welen1911 <welenalmeida2210@gmail.com>
2024-01-01 18:46:52 -0300

    Feat: delete produto

--- src/components/table.js
+++ src/components/table.js
@@ -5,12 +5,26 @@ export const Table = () => {
 
     const [produtos, setProdutos] = useState([]);
 
-    const [edit, setEdit] = useState(false);
+    const [edit, setEdit] = useState([false]);
+
+    const destroy = (id) => {
+        axios.delete('http://localhost:8000/api/' + id).then(
+            response => setProdutos(response.data)
+        ).catch(error => console.log(error))
+    }
 
     useEffect(() => {
         axios.get('http://localhost:8000/api/')
-        .then(response => setProdutos(response.data))
-        .catch(error => console.log(error));
+            .then(response => {
+                setProdutos(response.data);
+                // setProdutos(produto => [...produto.map(pro => pro.edit = false)]);
+            })
+            .catch(error => console.log(error));
+            let editProdutos = produtos;
+            editProdutos.map(produto => {
+                produto.edit = false;
+            });
+            setProdutos(editProdutos);
     }, []);
 
     return (
@@ -36,30 +50,31 @@ export const Table = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {produtos.map(produto => (
+                    {produtos.map((produto, index) => (
                         <tr key={produto.id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            {!edit ? produto.name : 
-                                <input key={produto.key} value={produto.name} className="dark:text-black"/>
-                            }
-                        </th>
-                        <td className="px-6 py-4">
-                            Silver
-                        </td>
-                        <td className="px-6 py-4">
-                            Laptop
-                        </td>
-                        <td className="px-6 py-4">
-                            $2999
-                        </td>
-                        <td className="px-6 py-4">
-                            <button onClick={() => {
-                                setEdit(!edit);
-                            }} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{edit ? "Verdadeiro" : "Falso"}</button>
-                        </td>
-                    </tr>
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {!produto.edit ? produto.name :
+                                    <input key={produto.key} value={produto.name} className="dark:text-black" />
+                                }
+                            </th>
+                            <td className="px-6 py-4">
+                                Silver
+                            </td>
+                            <td className="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td className="px-6 py-4">
+                                $2999
+                            </td>
+                            <td className="px-6 py-4">
+                                <button onClick={() => 
+                                    setProdutos([...produtos, produto.edit = !produto.edit])
+                                } className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</button>
+                                <button onClick={() => destroy(produto.id)} className="ml-3 font-medium dark:text-red-500 hover:underline">Deletar</button>
+                            </td>
+                        </tr>
                     ))}
-                    
+
                 </tbody>
             </table>
         </div>
